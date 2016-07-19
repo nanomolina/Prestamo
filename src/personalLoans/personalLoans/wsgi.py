018f159b64e7bfd4be2cb04ae808979ef6f46722
nanomolina <nanomolinacav@gmail.com>
2016-07-19 20:50:20 -0300

    comunicion corregida django + angular

--- src/personalLoans/personalLoans/wsgi.py
+++ src/personalLoans/personalLoans/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for PersonalLoans project.
+WSGI config for personalLoans project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PersonalLoans.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personalLoans.settings")
 
 application = get_wsgi_application()
