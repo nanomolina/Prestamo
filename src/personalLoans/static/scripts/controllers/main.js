619a78ef92819115025bf327132adc2b6b21f452
nanomolina <nanomolinacav@gmail.com>
2016-07-26 01:19:37 -0300

    main

--- src/personalLoans/static/scripts/controllers/main.js
+++ src/personalLoans/static/scripts/controllers/main.js
@@ -2,53 +2,53 @@
 
 angular.module('angularDjangoRegistrationAuthApp')
   .controller('MainCtrl', function ($scope, $cookies, $location, djangoAuth) {
-    
+
     $scope.login = function(){
       djangoAuth.login(prompt('Username'),prompt('password'))
       .then(function(data){
         handleSuccess(data);
       },handleError);
     }
-    
+
     $scope.logout = function(){
       djangoAuth.logout()
       .then(handleSuccess,handleError);
     }
-    
+
     $scope.resetPassword = function(){
       djangoAuth.resetPassword(prompt('Email'))
       .then(handleSuccess,handleError);
     }
-    
+
     $scope.register = function(){
       djangoAuth.register(prompt('Username'),prompt('Password'),prompt('Email'))
       .then(handleSuccess,handleError);
     }
-    
+
     $scope.verify = function(){
       djangoAuth.verify(prompt("Please enter verification code"))
       .then(handleSuccess,handleError);
     }
-    
+
     $scope.goVerify = function(){
       $location.path("/verifyEmail/"+prompt("Please enter verification code"));
     }
-    
+
     $scope.changePassword = function(){
       djangoAuth.changePassword(prompt("Password"), prompt("Repeat Password"))
       .then(handleSuccess,handleError);
     }
-    
+
     $scope.profile = function(){
       djangoAuth.profile()
       .then(handleSuccess,handleError);
     }
-    
+
     $scope.updateProfile = function(){
       djangoAuth.updateProfile({'first_name': prompt("First Name"), 'last_name': prompt("Last Name"), 'email': prompt("Email")})
       .then(handleSuccess,handleError);
     }
-    
+
     $scope.confirmReset = function(){
       djangoAuth.confirmReset(prompt("Code 1"), prompt("Code 2"), prompt("Password"), prompt("Repeat Password"))
       .then(handleSuccess,handleError);
@@ -57,21 +57,88 @@ angular.module('angularDjangoRegistrationAuthApp')
     $scope.goConfirmReset = function(){
       $location.path("/passwordResetConfirm/"+prompt("Code 1")+"/"+prompt("Code 2"))
     }
-    
+
     var handleSuccess = function(data){
       $scope.response = data;
     }
-    
+
     var handleError = function(data){
       $scope.response = data;
     }
 
-    $scope.show_login = true;
+    $scope.logged = true;
     $scope.$on("djangoAuth.logged_in", function(data){
-      $scope.show_login = false;
+      $scope.logged = false;
     });
     $scope.$on("djangoAuth.logged_out", function(data){
-      $scope.show_login = true;
+      $scope.logged = true;
     });
 
   });
+
+  angular.module('angularDjangoRegistrationAuthApp')
+      .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+        $scope.toggleLeft = buildDelayedToggler('left');
+        $scope.toggleRight = buildToggler('right');
+        $scope.isOpenRight = function(){
+          return $mdSidenav('right').isOpen();
+        };
+        /**
+         * Supplies a function that will continue to operate until the
+         * time is up.
+         */
+        function debounce(func, wait, context) {
+          var timer;
+          return function debounced() {
+            var context = $scope,
+                args = Array.prototype.slice.call(arguments);
+            $timeout.cancel(timer);
+            timer = $timeout(function() {
+              timer = undefined;
+              func.apply(context, args);
+            }, wait || 10);
+          };
+        }
+        /**
+         * Build handler to open/close a SideNav; when animation finishes
+         * report completion in console
+         */
+        function buildDelayedToggler(navID) {
+          return debounce(function() {
+            // Component lookup should always be available since we are not using `ng-if`
+            $mdSidenav(navID)
+              .toggle()
+              .then(function () {
+                $log.debug("toggle " + navID + " is done");
+              });
+          }, 200);
+        }
+        function buildToggler(navID) {
+          return function() {
+            // Component lookup should always be available since we are not using `ng-if`
+            $mdSidenav(navID)
+              .toggle()
+              .then(function () {
+                $log.debug("toggle " + navID + " is done");
+              });
+          }
+        }
+      })
+      .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+        $scope.close = function () {
+          // Component lookup should always be available since we are not using `ng-if`
+          $mdSidenav('left').close()
+            .then(function () {
+              $log.debug("close LEFT is done");
+            });
+        };
+      })
+      .controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+        $scope.close = function () {
+          // Component lookup should always be available since we are not using `ng-if`
+          $mdSidenav('right').close()
+            .then(function () {
+              $log.debug("close RIGHT is done");
+            });
+        };
+      });
