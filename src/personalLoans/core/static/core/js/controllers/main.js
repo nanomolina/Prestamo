a507b049268665bbc4bedec8823d4fd84fcd4111
Nano Molina <nanomolinacav@gmail.com>
2016-08-10 13:22:09 -0300

    profile http request

--- src/personalLoans/core/static/core/js/controllers/main.js
+++ src/personalLoans/core/static/core/js/controllers/main.js
@@ -1,70 +1,8 @@
 'use strict';
 
-app.controller('MainCtrl', function ($scope, $cookies, $location, djangoAuth, $mdSidenav) {
+app.controller('MainCtrl', function ($scope, $http, $cookies, $location, djangoAuth, $mdSidenav) {
 
-    $scope.login = function(){
-      djangoAuth.login(prompt('Username'),prompt('password'))
-      .then(function(data){
-        handleSuccess(data);
-      },handleError);
-    }
-
-    $scope.logout = function(){
-      djangoAuth.logout()
-      .then(handleSuccess,handleError);
-    }
-
-    $scope.resetPassword = function(){
-      djangoAuth.resetPassword(prompt('Email'))
-      .then(handleSuccess,handleError);
-    }
-
-    $scope.register = function(){
-      djangoAuth.register(prompt('Username'),prompt('Password'),prompt('Email'))
-      .then(handleSuccess,handleError);
-    }
-
-    $scope.verify = function(){
-      djangoAuth.verify(prompt("Please enter verification code"))
-      .then(handleSuccess,handleError);
-    }
-
-    $scope.goVerify = function(){
-      $location.path("/verifyEmail/"+prompt("Please enter verification code"));
-    }
-
-    $scope.changePassword = function(){
-      djangoAuth.changePassword(prompt("Password"), prompt("Repeat Password"))
-      .then(handleSuccess,handleError);
-    }
-
-    $scope.profile = function(){
-      djangoAuth.profile()
-      .then(handleSuccess,handleError);
-    }
-
-    $scope.updateProfile = function(){
-      djangoAuth.updateProfile({'first_name': prompt("First Name"), 'last_name': prompt("Last Name"), 'email': prompt("Email")})
-      .then(handleSuccess,handleError);
-    }
-
-    $scope.confirmReset = function(){
-      djangoAuth.confirmReset(prompt("Code 1"), prompt("Code 2"), prompt("Password"), prompt("Repeat Password"))
-      .then(handleSuccess,handleError);
-    }
-
-    $scope.goConfirmReset = function(){
-      $location.path("/passwordResetConfirm/"+prompt("Code 1")+"/"+prompt("Code 2"))
-    }
-
-    var handleSuccess = function(data){
-      $scope.response = data;
-    }
-
-    var handleError = function(data){
-      $scope.response = data;
-    }
-
+    //============================
     $scope.logged = true;
     $scope.$on("djangoAuth.logged_in", function(data){
       $scope.logged = false;
@@ -72,8 +10,6 @@ app.controller('MainCtrl', function ($scope, $cookies, $location, djangoAuth, $m
     $scope.$on("djangoAuth.logged_out", function(data){
       $scope.logged = true;
     });
-
-
     //=============================
     $scope.isSidenavOpen = false;
 
@@ -104,7 +40,15 @@ app.controller('MainCtrl', function ($scope, $cookies, $location, djangoAuth, $m
   });
 
 
-  app.controller('PrincipalCtrl', function ($scope, $mdSidenav) {
+  app.controller('PrincipalCtrl', function ($scope, $mdSidenav, $http) {
+
+    //=============================
+    $http.get("rest-auth/user")
+    .then(function(response) {
+        $scope.profile = response.data;
+    });
+    //=============================
+
     $scope.isSidenavOpen = false;
 
     $scope.openLeftMenu = function() {
@@ -123,6 +67,7 @@ app.controller('MainCtrl', function ($scope, $cookies, $location, djangoAuth, $m
         icon: 'group'
       },
     ];
+
     $scope.admin = [
       {
         link: '#/logout',
