61d67d83ae454adb704371db1a25e63467716cd4
Nano Molina <nanomolinacav@gmail.com>
2016-09-27 16:51:35 -0300

    redirect to item

--- src/personalLoans/entity/static/entity/js/controllers/console.js
+++ src/personalLoans/entity/static/entity/js/controllers/console.js
@@ -2,13 +2,23 @@
 
 
 app.controller('ConsoleCtrl', ConsoleCtrl);
-ConsoleCtrl.$inject = [];
+ConsoleCtrl.$inject = ['$scope', '$location'];
 
-function ConsoleCtrl() {
+function ConsoleCtrl($scope, $location) {
     var vm = this;
 
     vm.view = {
       title: 'Console',
       icon: 'view_list'
     };
+    vm.goToDetail = goToDetail;
+
+
+    // PUBLIC FUNCTIONS
+    function goToDetail() {
+      var id = $scope.params.associationId;
+      $location.path('/association/' + id + '/detail');
+    }
+
+    // PRIVATE FUNCTIONS
 }
