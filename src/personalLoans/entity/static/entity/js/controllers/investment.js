cee8f3609d52067ac02bef1720b20e1c4fee5184
Nano Molina <nanomolinacav@gmail.com>
2016-10-05 10:21:43 -0300

    filter front

--- src/personalLoans/entity/static/entity/js/controllers/investment.js
+++ src/personalLoans/entity/static/entity/js/controllers/investment.js
@@ -2,9 +2,9 @@
 
 app.controller('InvestmentCtrl', InvestmentCtrl);
 
-InvestmentCtrl.$inject = ['investmentService', '$routeParams'];
+InvestmentCtrl.$inject = ['investmentService', '$routeParams', '$scope'];
 
-function InvestmentCtrl(investmentService, $routeParams) {
+function InvestmentCtrl(investmentService, $routeParams, $scope) {
     var vm = this;
 
     vm.view = {
@@ -13,17 +13,27 @@ function InvestmentCtrl(investmentService, $routeParams) {
     };
     vm.selected = [];
     vm.query = {
-       ordering: '-date',
-       limit: 5,
-       page: 1
+      filter: '',
+      ordering: '-date',
+      limit: 5,
+      page: 1
     };
     vm.investments = [];
-    vm.getInvestments = getInvestments;
     vm.promise;
+    vm.filter = {
+      show: false,
+      options: {
+        debounce: 500
+      },
+      form: undefined,
+    };
+    vm.getInvestments = getInvestments;
+    vm.showSearchFilter = showSearchFilter;
+    vm.removeFilter = removeFilter;
 
     getInvestments();
 
-    // PRIVATE FUNCTIONS
+    // PUBLIC FUNCTIONS
     function getInvestments() {
       var id = $routeParams.associationId;
       vm.promise = investmentService.getList(id, vm.query)
@@ -31,4 +41,31 @@ function InvestmentCtrl(investmentService, $routeParams) {
         vm.investments = response.data;
       });
     }
+
+    function showSearchFilter() {
+      vm.filter.show = true;
+    }
+
+    function removeFilter() {
+      vm.filter.show = false;
+      vm.query.filter = '';
+      if(vm.filter.form.$dirty) {
+        vm.filter.form.$setPristine();
+      }
+    }
+
+    // PRIVATE FUNCTIONS
+    $scope.$watch('vm.query.filter', function (newValue, oldValue) {
+      var bookmark = 1;
+      if(!oldValue) {
+        bookmark = vm.query.page;
+      }
+      if(newValue !== oldValue) {
+        vm.query.page = 1;
+      }
+      if(!newValue) {
+        vm.query.page = bookmark;
+      }
+      vm.getInvestments();
+    });
 }
